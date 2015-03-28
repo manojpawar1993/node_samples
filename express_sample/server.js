386bfdb41ac4a192515b5ce30fc603526e882a80
Naik, Parag Laxman (US - Mumbai) <parnaik@deloitte.com>
2015-03-28 12:24:34 +0530

    express: employee list.

--- express_sample/server.js
+++ express_sample/server.js
@@ -2,9 +2,13 @@ var express = require('express');
 var server = express();
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var validator = require('express-validator');
+
+var emp_list = [];
 
 server.use(bodyParser.json()); // for parsing application/json
 server.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+server.use(validator());
 server.use(multer()); // for parsing multipart/form-data
 
 // server specific config
@@ -13,10 +17,37 @@ server.use(function(req, res, next) {
     next();
 });
 
+server.get('/employees', function(req, res){
+    res.json({list: emp_list});
+});
+
 // app specific routing
 server.post('/employees', function(req, res) {
-    //console.log(req.body);
-    res.send("hello " + req.body.name);
+    // console.log(req.body);
+    // console.log(req.query);
+    // res.send("ok");
+    // res.send("hello " + req.body.name);
+    // 
+    
+    //req.assert('email', 'required').notEmpty();
+    req.assert('email', 'valid email required.').isEmail();
+    // all your asserts here.
+    var errors = req.validationErrors();
+
+    if (errors) {
+        res.status(500);
+        res.send({ error: errors});
+        return;
+    }
+
+    var employee = {};
+    employee.name = req.body.name;
+    employee.phone = req.body.phone;
+    employee.email = req.body.email;
+
+    emp_list.push(employee);
+
+    res.json(employee);
 })
 
 server.listen(5555);
